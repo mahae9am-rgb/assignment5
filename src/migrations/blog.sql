b246f22f5aab572b6a8ea7df2d49252cef11f630
Maha Sakr <mahae9am@gmail.com>
2026-09-17 17:33:45 -0700

    Remove unused src/migrations folder

--- src/migrations/blog.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE blogs (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    content TEXT NOT NULL,
-    author_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
-);
