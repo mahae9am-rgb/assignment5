5be0df3e46a3ea675d0096e51c83cd1dc6cd5322
Maha Sakr <mahae9am@gmail.com>
2026-09-17 17:25:08 -0700

    Remove duplicate root-level migrations and models folders

--- src/app/auth/auth.route.js
+++
@@ -1,5 +0,0 @@
-const {Router}= require('express')
-const authcontroller= require('./auth.controller')
-const authRouter=Router()
-authRouter.post('/login',authcontroller.login)
-  module.exports=authRouter
